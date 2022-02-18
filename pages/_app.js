ee21ad6064ab3cd4302d5b0ec95a95779722a9e9
DiegoIpaez <diegokpo1313@gmail.com>
2022-02-18 13:00:43 -0300

    Añadiendo scss y dependencias

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import "../scss/global.scss";
+import "semantic-ui-css/semantic.min.css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
